fa10a33cb8ebc2136bf213e285d5bbc120b82ca8
Rayflex189 <roryisaiah66@gmail.com>
2025-02-20 05:08:40 -1100

    UPDATED

--- shipping_site/shipping_site/settings.py
+++ shipping_site/shipping_site/settings.py
@@ -14,6 +14,23 @@ from pathlib import Path
 import os
 
 
+import dj_database_url
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'swiftship_dbs',
+        'USER': 'swiftship_dbs_user',
+        'PASSWORD': 'KhFiwR0GiHhkNfz84fPns9bBcXLTRZbf',
+        'HOST': 'dpg-curkuaa3esus73dq18kg-a.oregon-postgres.render.com',  # Check this
+        'PORT': '5432',
+    },
+     'OPTIONS': {
+            'sslmode': 'require',
+        },
+}
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,17 +93,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'shipping_site.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
